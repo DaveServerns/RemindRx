4e6a267dc06a09a28d467536b6d0aaaa8e4fa213
illphil610 <phil.cappelli@gmail.com>
2017-02-25 12:51:15 -0500

    Updated layout

--- app/src/main/java/edu/newwesterndev/remindrx/MainActivity.java
+++ app/src/main/java/edu/newwesterndev/remindrx/MainActivity.java
@@ -2,12 +2,37 @@ package edu.newwesterndev.remindrx;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    View parentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parentLayout = findViewById(R.id.activity_main);
+
+        ListView pillList = (ListView) findViewById(R.id.pillList);
+
+        ArrayList<String> daysOfWeek = new ArrayList<>();
+        daysOfWeek.add("Sunday");
+        daysOfWeek.add("Monday");
+        daysOfWeek.add("Tuesday");
+        daysOfWeek.add("Wednesday");
+        daysOfWeek.add("Thursday");
+        daysOfWeek.add("Friday");
+        daysOfWeek.add("Saturday");
+
+        PillAdapter pillAdapter = new PillAdapter(this, daysOfWeek);
+
+        pillList.setAdapter(pillAdapter);
+
+
     }
 }
---
+++ app/src/main/java/edu/newwesterndev/remindrx/PillAdapter.java
@@ -0,0 +1,58 @@
+package edu.newwesterndev.remindrx;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by phil on 2/25/17.
+ */
+
+public class PillAdapter extends BaseAdapter{
+
+    Context context;
+    ArrayList<String> daysOfTheWeek;
+
+    public PillAdapter(Context context, ArrayList<String> daysOfTheWeek){
+
+        this.context = context;
+        this.daysOfTheWeek = daysOfTheWeek;
+    }
+
+    @Override
+    public int getCount() {
+        return daysOfTheWeek.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return daysOfTheWeek.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView = new TextView(context);
+        textView.setText(daysOfTheWeek.get(position));
+
+        return textView;
+    }
+
+
+
+
+
+
+
+
+
+
+}
